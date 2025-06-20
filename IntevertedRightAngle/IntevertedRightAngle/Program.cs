2d5b7405a95289f0c5aeb3d79aa2c98359e8d3b0
DEV-HEMANTH <hemanthreddy@scita.in>
2025-06-20 18:26:03 +0530

    IntevertedRightAngle commit

---
+++ IntevertedRightAngle/IntevertedRightAngle/Program.cs
@@ -0,0 +1,18 @@
+﻿class program
+{
+    static void printInvertedRightAngle(int n)
+    {
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+    public static void Main(String[] args)
+    {
+        printInvertedRightAngle(4);
+    }
+}
