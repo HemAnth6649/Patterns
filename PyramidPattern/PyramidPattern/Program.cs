2ce0e56b4e52dbcb0d749990364167a3053748b9
DEV-HEMANTH <hemanthreddy@scita.in>
2025-06-20 18:22:56 +0530

    added PyramidPattern as normal folders

---
+++ PyramidPattern/PyramidPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿class pyramid
+{
+    static void PrintPyramid(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int space = 1; space <=n - i; space++)
+            {
+                Console.Write(" ");
+            }
+            for (int star = 1; star <= 2 * i - 1; star++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+    public static void Main(string[] args)
+    {
+        PrintPyramid(5);
+    }
+}
