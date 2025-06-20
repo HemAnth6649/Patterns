56887b956f4f5f5cf513f2f947159a57e6b3a421
DEV-HEMANTH <hemanthreddy@scita.in>
2025-06-20 18:27:33 +0530

    InvertedPyramid commit

---
+++ InvertedPyramid/InvertedPyramif/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void printInvertedPyramid(int n)
+    {
+       for(int i = 0; i < n; i++)
+       {
+            // Print leading spaces
+            for (int space = 0; space < i; space++)
+            {
+                Console.Write(" ");
+            }
+
+            // Print stars
+            int stars = 2 * (n - i) - 1;
+            for (int star = 0; star < stars; star++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine(); // Move to next line
+        }
+    
+    }
+    static void Main(string[] args)
+    {
+        printInvertedPyramid(5);
+    }
+}
