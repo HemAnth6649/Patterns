c23e612aeb2bfbb9feafef30b1c03e06f87bd132
DEV-HEMANTH <hemanthreddy@scita.in>
2025-06-20 18:30:17 +0530

    RightAngledTriangle commit

---
+++ RightAngledTriangle/RightAngledTriangle/Program.cs
@@ -0,0 +1,18 @@
+﻿class program
+{
+    static void PrintRightAngledTriangle(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++) 
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+    public static void Main(string[] args)
+    {
+        PrintRightAngledTriangle(5);
+    }
+}
